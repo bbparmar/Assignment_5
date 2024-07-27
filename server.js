b33e6bd6eb685156802fe1f10add162848140482
bbparmar <bbparmar@myseneca.ca>
2024-07-26 22:18:50 -0400

    update

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const data = require("./modules/collegeData.js");
 
 const app = express();
 
-const HTTP_PORT = "https://assignment-5-self.vercel.app/";
+const HTTP_PORT = process.env.PORT || 8080;
 
 // Configure Handlebars
 app.engine(".hbs", exphbs.engine({
